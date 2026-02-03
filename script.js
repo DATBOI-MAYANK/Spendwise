c98ab590fc5c46b49ac68a0730a6c735eeb3ab4f
Mayank Roy <mayankroy106106@gmail.com>
2026-02-03 20:01:19 +0530

    feat:Add_Expense_list_Input

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 let budget = document.querySelector("#budget");
-let add = document.querySelector("#add");
+let addBtn = document.querySelector("#add");
 let confirm = document.querySelector("#confirm");
 let modal = document.querySelector("#modal");
 let amount = document.querySelector("#amount");
-add.addEventListener("click", () => {
+let expenseModal = document.querySelector("#expenseModal");
+let expenseBtn = document.querySelector("#expense");
+let price = document.querySelector("#price");
+let item = document.querySelector("#item");
+let addItem = document.querySelector("#addItem");
+
+expenseBtn.addEventListener("click", () => {
+  price.style.display = "block";
+  item.style.display = "block";
+  addItem.style.display = "block";
+  expenseModal.style.position = "absolute";
+});
+
+addItem.addEventListener("click", () => {
+  if (!price.value && !item.value) {
+    alert("Item and Price cannot be empty!!");
+  } else {
+    price.style.display = "none";
+    item.style.display = "none";
+    price.value = "";
+    item.value = "";
+    addItem.style.display = "none";
+    expenseModal.style.position = "";
+  }
+});
+
+addBtn.addEventListener("click", () => {
   budget.style.display = "block";
   confirm.style.display = "block";
   modal.style.position = "absolute";
 });
 
 confirm.addEventListener("click", () => {
-  if (!budget.value) alert("Budget cannot be empty!!");
-  else {
+  if (!budget.value) {
+    alert("Budget cannot be empty!!");
+  } else {
     let value = budget.value.replace(/,/g, "");
     budget.style.display = "none";
     confirm.style.display = "none";
